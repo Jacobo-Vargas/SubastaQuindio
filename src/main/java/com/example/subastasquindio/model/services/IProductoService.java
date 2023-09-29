8440ac9393914400375d8b4cf19b65aeec8960bc
jacobo <jacobo.vargasg@uqvirtual.edu.co>
2023-09-28 20:24:03 -0500

    adelanto CRUD producto

--- src/main/java/com/example/subastasquindio/model/services/IProductoService.java
+++ src/main/java/com/example/subastasquindio/model/services/IProductoService.java
@@ -1,7 +1,6 @@
 package com.example.subastasquindio.model.services;
 
 import com.example.subastasquindio.exceptions.ProductoException;
-import com.example.subastasquindio.exceptions.UsuarioException;
 import com.example.subastasquindio.model.Producto;
 import com.example.subastasquindio.model.TipoArticulo;
 
